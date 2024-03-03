cc2118cb87a038ac0dbe0fa41ef8b8e068ead5a7
Adrian Pinkowicz <apinko@outlook.com>
2024-03-03 12:44:56 +0100

    my-app (pre button)

---
+++ app/src/main/java/com/example/myapplication_03/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication_03
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+import android.widget.Button
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var clickCount = 0
+    private lateinit var textView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.TextView)
+
+        val button = findViewById<Button>(R.id.Button)
+
+        button.setOnClickListener {
+            clickCount++
+            updateClickCount()
+        }
+
+       /* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets*/
+        }
+    private fun updateClickCount() {
+        textView.text = "Liczba kliknięć: $clickCount"
+    }
+
+    }
+}
