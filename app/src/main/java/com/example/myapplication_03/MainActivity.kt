9d9124ee8c24c4f8244668c2d580d4c406634d18
Adrian Pinkowicz <apinko@outlook.com>
2024-03-03 12:56:36 +0100

    my-app (ok))

--- app/src/main/java/com/example/myapplication_03/MainActivity.kt
+++ app/src/main/java/com/example/myapplication_03/MainActivity.kt
@@ -3,8 +3,7 @@ package com.example.myapplication_03
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 import android.widget.Button
 import android.widget.TextView
@@ -39,4 +38,3 @@ class MainActivity : AppCompatActivity() {
     }
 
     }
-}
